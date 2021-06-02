57609c1127f2499db5f66eef63d278183abb223a
KCsup <kcsup05@gmail.com>
2021-06-02 17:52:00 -0400

    Add project files.

---
+++ CriticalOverdriveServer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CriticalOverdriveServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Critical Overdrive Server v1.0";
+            Server.Start(20, 8280);
+            Console.ReadKey();
+        }
+    }
+}
